542f7535b04651feb65a0600e520355617c6d662
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2018-02-26 12:20:14 +0100

    Création variable globale pour todolist pour contenir tableau des tâches. Ajout des tâches ok, suppression des tâches ok, affichage ok, problème au rafraichissement de la page, toute la liste des tâches disparait

--- OpenClassrooms - Des applications ultra-rapides avec Node.js/Todolist_tempsreel/app.js
+++ OpenClassrooms - Des applications ultra-rapides avec Node.js/Todolist_tempsreel/app.js
@@ -9,61 +9,80 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var ent = require('ent'); //Charge module ent pour éviter échange JavaScript malicieux en échappant caractère HTML (sécurité équivalente à htmlentities en PHP)
 var fs = require('fs'); // Charge module extension inclus dans la librairie nodejs (fs = file system). Permet de lire de façon asynchrone tout le contenu d'un fichier
 
-/* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
+// On créer une variable globale (accessible de partout) coté serveur (dans fichier app.js) contenant un tableau reprenant toutes les tâches de la todolist
+// C'est ce tableau qui sera envoyé à tous les utilisateurs (broadcast), c'est également à ce tableau que seront ajouté les nouvelles tâches avec push et retirer les tâches avec splice
+// var todolist = ['Repasser le linge', 'Faire la vaisselle', 'Faire les courses', 'Cuisiner le repas'];
+// Pas nécessaire de créer un todolist vide pour éviter les erreur javascript lorsque on la stocke dans une variable.
+// Il suffit de créer au départ un tableau vide en indiquant []. Le tableau se remplira ensuite au fil des tâches ajoutées avec push et supprimée avec splice
+var todolist = [];
 
 
-/* S'il n'y a pas de todolist dans la session,
-on en crée une vide sous forme d'array avant la suite */
-app.use(function(req, res, next){
-    if (typeof(req.session.todolist) == 'undefined') {
-        req.session.todolist = [];
-    }
-    next();
-})
-
-
-/* On affiche la todolist et le formulaire */
+/* On affiche la todolist et le formulaire avec express en récupérant la vue todo.ejs*/
 app.get('/todo', function(req, res) {
-    res.render('todo.ejs', {todolist: req.session.todolist});
-})
+    res.render('todo.ejs', {todolist: todolist});
+});
 
-// Quand un client se connecte, il récupère la dernière Todolist connue du serveur
-// Le serveur peut retenir la Todolist sous la forme d'un simple array qu'il gardera en mémoire.
-// La persistence n'est pas demandée (inutile d'utiliser MySQL ou Mongodb)
+// /* On utilise les sessions */
+// app.use(session({secret: 'todotopsecret'}))
 
-io.sockets.on('connection', function (socket, pseudo) { //connection à socket.io
 
-  // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-  socket.on('nouveau_client', function(pseudo) {
-      pseudo = ent.encode(pseudo);// On utilise le module ent pour échapper tout les caractère html (on transforme ces caractère pour les rendre inoffensifs) du type < > etc... pour n'avoir qu'un contenu en pure texte
-      socket.pseudo = pseudo;// On sauvegarde le pseudo dans une variable de session
-      socket.broadcast.emit('nouveau_client', pseudo);// Le serveur envoi à tous les clients connectés un message de type nouveau_client qui contient le pseudo du client qui vient de se connecté (pseudo)
-  });
+// // Si la todolist est vide quand l'utilisateur se connecte au départ,
+// // on crée une todolist vide sous forme d'un array vide afin d'éviter une erreur javascript qui n'aime pas parcourir des tableaus inexistants */
+// app.use(function(req, res, next){
+//     if (typeof(todolist) == 'undefined') {
+//         todolist = [];
+//     }
+//     next();
+// })
 
   /* On ajoute un élément à la todolist */
   app.post('/todo/ajouter/', urlencodedParser, function(req, res) {
       if (req.body.newtodo != '') {
-          req.session.todolist.push(req.body.newtodo);
+          todolist.push(req.body.newtodo);
       }
       res.redirect('/todo');
   })
 
-  // Quand un client ajoute une tâche, celle-ci est immédiatment répercutée chez les autres clients
+    /* Supprime un élément de la todolist */
+    app.get('/todo/supprimer/:id', function(req, res) {
+        if (req.params.id != '') {
+            todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
+
+    console.log(todolist[1]);
+
+    /* On redirige vers la todolist si la page demandée n'est pas trouvée */
+    app.use(function(req, res, next){
+        res.redirect('/todo');
+    })
 
 
+// // Quand un client se connecte, il récupère la dernière Todolist connue du serveur
+// // Le serveur peut retenir la Todolist sous la forme d'un simple array qu'il gardera en mémoire.
+// // La persistence n'est pas demandée (inutile d'utiliser MySQL ou Mongodb)
+//
+// io.sockets.on('connection', function (socket, pseudo) { //connection à socket.io
+//
+//   // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+//   socket.on('nouveau_client', function(pseudo) {
+//       pseudo = ent.encode(pseudo);// On utilise le module ent pour échapper tout les caractère html (on transforme ces caractère pour les rendre inoffensifs) du type < > etc... pour n'avoir qu'un contenu en pure texte
+//       socket.pseudo = pseudo;// On sauvegarde le pseudo dans une variable de session
+//       socket.broadcast.emit('nouveau_client', pseudo);// Le serveur envoi à tous les clients connectés un message de type nouveau_client qui contient le pseudo du client qui vient de se connecté (pseudo)
+//   });
+//
 
+//
+//   // Quand un client ajoute une tâche, celle-ci est immédiatment répercutée chez les autres clients
+//
+//
+//
+//
 
-  /* Supprime un élément de la todolist */
-  app.get('/todo/supprimer/:id', function(req, res) {
-      if (req.params.id != '') {
-          req.session.todolist.splice(req.params.id, 1);
-      }
-      res.redirect('/todo');
-  })
-
-  // Quand un client supprimer une tâche, celle-ci est immédiatment supprimée chez les autres clients
-
+//
+//   // Quand un client supprimer une tâche, celle-ci est immédiatment supprimée chez les autres clients
+//
 
 
   /* On redirige vers la todolist si la page demandée n'est pas trouvée */
@@ -71,7 +90,7 @@ io.sockets.on('connection', function (socket, pseudo) { //connection à socket.i
       res.redirect('/todo');
   })
 
-});
+// });
 
 
 
